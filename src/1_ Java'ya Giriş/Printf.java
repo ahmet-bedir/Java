7b56a3fa1c7aeb0c8ceb97f8ac2daf286e7c7c71
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-08-28 12:22:36 +0300

    güncelleme işlemi yapıldı.

---
+++ src/1_ Java'ya Giriş/Printf.java
@@ -0,0 +1,25 @@
+public class Printf {
+    public static void main(String[] args) {
+        String isim = "Ali";
+        int yas = 25;
+        double not = 3.75;
+
+        System.out.printf("Ad: %s, Yaş: %d, Not: %.2f%n", isim, yas, not);
+    }
+}
+/*
+println — Yazdırdıktan sonra, alt satıra geçer
+print — Yazdırdıktan sonra, alt satıra geçmez
+
+printf — Formatlı yazdırma
+
+%s → String (metin)
+
+%d → Integer (tam sayı)
+
+%f → Float/Double (ondalıklı sayı)
+
+%.2f → Ondalıklı sayı, virgülden sonra 2 basamak
+
+%n → Yeni satır (platform bağımsız)
+*/
---
+++ src/1_ Java'ya Giriş/UygArgs.java
@@ -0,0 +1,39 @@
+// args argümanı programa dışarıdan değer göndermeni sağlar.
+
+public class UygArgs {
+    public static void main(String[] args) {
+    /*
+        if (args.length > 0) {
+            System.out.println("Merhaba, " + args[0] + "!");
+        } else {
+            System.out.println("Merhaba, yabancı!");
+        }
+
+        $ javac UygArgs.java
+
+        $ java UygArgs Ali
+        # Çıktı: Merhaba, Ali!
+
+        $ java UygArgs
+        # Çıktı: Merhaba, yabancı!
+
+        args[0] ilk argümanı,
+        args[1] ikinci argümanı verir.
+        args.length toplam argüman sayısını döndürür.
+    */
+
+        // Birden fazla argüman gönderme
+        System.out.println("Toplam argüman: " + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
+        // $ javac UygArgs.java
+        // $ java UygArgs Java çok güzel
+        // Çıktı:
+        // Toplam argüman: 3
+        // args[0] = Java
+        // args[1] = çok
+        // args[2] = güzel
+
+    }
+}
