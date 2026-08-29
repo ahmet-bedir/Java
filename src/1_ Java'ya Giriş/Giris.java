6fa55e7d21fdf02044722356d1e11c210f7ad57f
Termux <community@termux.com>
2026-08-29 15:57:42 +0300

    Otomatik Güncelleme İşlemi Yapıldı.

--- src/1_ Java'ya Giriş/Giris.java
+++ src/1_ Java'ya Giriş/Giris.java
@@ -2,6 +2,7 @@ public class Giris {
     static void main(String[] args) {
         String dil = "Java";
         int yas = 30;
+
         System.out.println(dil + " programlama dili " + yas + " yaşında!");
     }
 }
--- src/1_ Java'ya Giriş/UygArgs.java
+++ src/1_ Java'ya Giriş/UygArgs.java
@@ -2,13 +2,15 @@
 
 public class UygArgs {
     public static void main(String[] args) {
-    /*
         if (args.length > 0) {
             System.out.println("Merhaba, " + args[0] + "!");
         } else {
             System.out.println("Merhaba, yabancı!");
         }
+    }
+}
 
+/*
         $ javac UygArgs.java
 
         $ java UygArgs Ali
@@ -22,18 +24,23 @@ public class UygArgs {
         args.length toplam argüman sayısını döndürür.
     */
 
-        // Birden fazla argüman gönderme
+// Birden fazla argüman gönderme.
+/*
+public class UygArgs {
+    public static void main(String[] args) {
         System.out.println("Toplam argüman: " + args.length);
         for (int i = 0; i < args.length; i++) {
             System.out.println("args[" + i + "] = " + args[i]);
         }
-        // $ javac UygArgs.java
-        // $ java UygArgs Java çok güzel
-        // Çıktı:
-        // Toplam argüman: 3
-        // args[0] = Java
-        // args[1] = çok
-        // args[2] = güzel
-
     }
 }
+*/
+/*
+        $ javac UygArgs.java
+        $ java UygArgs Java çok güzel
+        # Çıktı:
+        # Toplam argüman: 3
+        # args[0] = Java
+        # args[1] = çok
+        # args[2] = güzel
+*/
