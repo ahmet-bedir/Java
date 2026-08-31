75eb5875a33e09856e93ee2335c2419d2ad28219
Termux <community@termux.com>
2026-08-31 22:30:39 +0300

    Otomatik Güncelleme İşlemi Yapıldı.

---
+++ src/1_ Java'ya Giriş/KisiselKart.java
@@ -0,0 +1,10 @@
+public class KisiselKart {
+    public static void main(String[] args) {
+        System.out.println("╔═══════════════════════════════╗");
+        System.out.println("║  İSİM: Ali Yılmaz             ║");
+        System.out.println("║  YAŞ: 22                      ║");
+        System.out.println("║  BÖLÜM: Bilgisayar Mühendisi  ║");
+        System.out.println("║  HOBBY: Coding                ║");
+        System.out.println("╚═══════════════════════════════╝");
+    }
+}
