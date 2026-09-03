fa3ab94eeb931ec1631d21d2f8350c07d7430324
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-09-03 11:49:06 +0300

    Otomatik Güncelleme İşlemi Yapıldı.

---
+++ src/1_ Java'ya Giriş/DegiskenOrnekleri.java
@@ -0,0 +1,15 @@
+public class DegiskenOrnekleri {
+    public static void main(String[] args) {
+        int yas = 25;              // Tam sayı
+        double boy = 1.75;         // Ondalıklı sayı
+        String isim = "Ali";       // Metin
+        boolean ogrenciMi = true;  // Doğru/Yanlış
+        char harf = 'A';           // Tek karakter
+
+        System.out.println("İsim: " + isim);
+        System.out.println("Yaş: " + yas);
+        System.out.println("Boy: " + boy);
+        System.out.println("Öğrenci mi: " + ogrenciMi);
+        System.out.println("İlk harf: " + harf);
+    }
+}
