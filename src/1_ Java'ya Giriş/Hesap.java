e19b026dd65293dc7ad4480cef1e01cc59f5d3eb
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-09-09 16:00:05 +0300

    Otomatik Güncelleme İşlemi Yapıldı.

--- src/1_ Java'ya Giriş/Hesap.java
+++ src/1_ Java'ya Giriş/Hesap.java
@@ -1,4 +1,4 @@
-public class BasitHesap {
+public class Hesap {
     public static void main(String[] args) {
         int a = 15;
         int b = 7;
---
+++ src/1_ Java'ya Giriş/Scanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;  // Scanner'ı import et
+
+public class Scanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Klavyeyi dinlemeye başla
+
+        System.out.print("Adın ne? ");  // Kullanıcıya sor
+        String isim = scanner.nextLine();  // Cevabı oku
+
+        System.out.println("Merhaba, " + isim + "!");  // Cevabı kullan
+
+        scanner.close();  // İşin bitince kapat
+    }
+}
