0592d34d2d57b16c6e9e06b102e6b7dd7a925d54
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-09-12 15:52:12 +0300

    Otomatik Güncelleme İşlemi Yapıldı.

--- src/1_ Java'ya Giriş/UygScanner.java
+++ src/1_ Java'ya Giriş/UygScanner.java
@@ -26,16 +26,16 @@ public class UygScanner {
 ---
 Scanner Metodları
 
-Metod	        Ne Okur	            Dönüş Tipi	Örnek Girdi
-nextLine()	    Satırın tamamını	String	    Merhaba Dünya
-next()	        Sonraki kelimeyi	String	    Merhaba
-nextInt()	    Tam sayı	        int	        42
-nextDouble()	Ondalıklı sayı	    double	    3.14
-nextLong()	    Büyük tam sayı	    long	    9000000000
-nextFloat()     Ondalıklı sayı	    float	    3.14
-nextBoolean()	Boolean	            boolean	    true
-hasNextInt()	Sıradaki int mi?	boolean	    —
-hasNextLine()	Başka satır var mı?	boolean 	—
+Metod	        Ne Okur	                Dönüş Tipi	Örnek Girdi
+nextLine()	    Satırın tamamını	    String	    Merhaba Dünya
+next()	        İlk kelimeyi	String	    Merhaba
+nextInt()	    Tam sayı	            int	        42
+nextDouble()	Ondalıklı sayı	        double	    3.14
+nextLong()	    Büyük tam sayı	        long	    9000000000
+nextFloat()     Ondalıklı sayı	        float	    3.14
+nextBoolean()	Boolean	                boolean	    true
+hasNextInt()	Sıradaki int mi?	    boolean	    —
+hasNextLine()	Başka satır var mı?	    boolean 	—
 
 örnek:
 
